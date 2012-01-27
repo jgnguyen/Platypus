3e09ff16c97d7aa67d31c677117a66233770a4cb
John Nguyen <jnguyen@coaps.fsu.edu>
2012-01-27 14:40:51 -0500

    Added setForIf and setIf methods.

---
+++ example/index.php
@@ -0,0 +1,13 @@
+<?php require_once('../platypus.php');
+
+$perry = new Platypus('index.html');
+
+$perry->set('Header', 'Platypus Example');
+$perry->set('Content', 'This is an example page templated using Platypus');
+
+$tableCols = array('Col1', 'Col2');
+$tableData = array(array('Data 1', 'Data 2'), array('Data 3', 'Data 4'));
+
+$perry->setFor('TableContent', 'tableData.html', $tableCols, $tableData);
+
+$perry->printHTML();
