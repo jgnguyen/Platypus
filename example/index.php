38c410d57bff677c787f8bde6eadbb5cae098011
John Nguyen <nguyen.john@me.com>
2012-01-28 10:09:25 -0500

    Moved some files around.

--- example/index.php
+++ example/index.php
@@ -1,6 +1,6 @@
 <?php require_once('../platypus.php');
 
-$perry = new Platypus('index.html');
+$perry = new Platypus('templates/index.html');
 
 $perry->set('Header', 'Platypus Example');
 $perry->set('Content', 'This is an example page templated using Platypus');
@@ -8,6 +8,6 @@ $perry->set('Content', 'This is an example page templated using Platypus');
 $tableCols = array('Col1', 'Col2');
 $tableData = array(array('Data 1', 'Data 2'), array('Data 3', 'Data 4'));
 
-$perry->setFor('TableContent', 'tableData.html', $tableCols, $tableData);
+$perry->setFor('TableContent', 'templates/tableData.html', $tableCols, $tableData);
 
 $perry->printHTML();
